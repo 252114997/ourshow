29bc9b74227f9e5750aa3e2910119e65d5c94ff4
root <252114997@qq.com>
2016-02-19 19:05:29 -0500

    1.correct data seeder

--- app/database/seeds/TbAblumPictureTableSeeder.php
+++ app/database/seeds/TbAblumPictureTableSeeder.php
@@ -15,135 +15,135 @@ class TbAblumPictureTableSeeder extends Seeder {
 			0 => 
 			array (
 				'ablum_id' => '1',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '9d377b10ce778c4938b3c7e2c63a229a',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			1 => 
 			array (
 				'ablum_id' => '1',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '2b04df3ecc1d94afddff082d139c6f15',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			2 => 
 			array (
 				'ablum_id' => '2',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'ac037a6becf9784759f2f9c4edaca16b',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			3 => 
 			array (
 				'ablum_id' => '2',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'f28af86aa2d562750a8261d72c3c4efa',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			4 => 
 			array (
 				'ablum_id' => '2',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'e9ef5afcf07d36a1afeca48977921b80',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			5 => 
 			array (
 				'ablum_id' => '3',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '2381933debf0f1b026ebf6f8b74014fc',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			6 => 
 			array (
 				'ablum_id' => '3',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'dd9b6433d9390bb01af27f223f927544',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			7 => 
 			array (
 				'ablum_id' => '3',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '6534ee6a4a0ba3188b367d02731af9e1',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			8 => 
 			array (
 				'ablum_id' => '3',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'e31371a35a3bb093ec6704b0911554bc',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			9 => 
 			array (
 				'ablum_id' => '4',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '6b226d546d28f4672203dca985936483',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			10 => 
 			array (
 				'ablum_id' => '4',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'd3a56bf4a81384533370122fcbeb6177',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			11 => 
 			array (
 				'ablum_id' => '4',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '5b2e615bcf385c2371c11adb3699b6c5',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			12 => 
 			array (
 				'ablum_id' => '4',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '2c67cac450f349cb61ad9e0a3a189e17',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			13 => 
 			array (
 				'ablum_id' => '4',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '0607c0ec7d87090c8dac6566c76da059',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			14 => 
 			array (
 				'ablum_id' => '5',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '8f1e23597b31d274efac6efef142b347',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			15 => 
 			array (
 				'ablum_id' => '5',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '5493313e140e16e7fa56196654e70624',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			16 => 
 			array (
 				'ablum_id' => '5',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '0552ad3fd717bf309b13600fb39c4707',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			17 => 
 			array (
 				'ablum_id' => '5',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => '7af2d3b9c8a120952daa6452389de2b4',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 			18 => 
 			array (
 				'ablum_id' => '5',
-				'picture_id' => '0',
-				'created_at' => '2016-02-19 22:22:37',
-				'updated_at' => '2016-02-19 22:22:37',
+				'picture_id' => 'f04fb15fbea409105cba1cbd927a38b5',
+				'created_at' => '2016-02-20 08:02:46',
+				'updated_at' => '2016-02-20 08:02:46',
 			),
 		));
 	}
