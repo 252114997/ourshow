652c12316642af6f85078405a0c29abd0879903b
252114997@qq.com <252114997@qq.com>
2016-02-23 22:53:53 +0800

    1.bug fix. correct tb_pictures model definition

--- app/models/tb_pictures.php
+++ app/models/tb_pictures.php
@@ -5,7 +5,7 @@ class tb_pictures extends Eloquent {
 	protected $table = 'tb_pictures';
 	
 	public $timestamps = true;
-	public $incrementing = true;
+	public $incrementing = false;
 	protected $primaryKey = "id";
 	// public $guarded = array('*');
 	protected $fillable = array(
